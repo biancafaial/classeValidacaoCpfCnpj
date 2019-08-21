3b59ded31df0d888d6a0151eae421502bd83d43b
biancafaial <biancafaial@gmail.com>
2019-08-21 09:34:39 -0300

    Classe de validação de CPF/CNPJ

---
+++ src/TestaCPFCNPJ.java
@@ -0,0 +1,13 @@
+
+public class TestaCPFCNPJ {
+
+	public static void main(String[] args) {
+		
+		//String retorno = ValidacaoCPF_CNPJ.execute("60.701.190/0067-22");
+		String retorno = ValidacaoCPF_CNPJ.execute("111.111.111-11");
+
+		System.out.println("Retorno = " + retorno);
+
+	}
+
+}
---
+++ src/ValidacaoCPF_CNPJ.java
@@ -0,0 +1,179 @@
+import java.util.InputMismatchException;
+
+public class ValidacaoCPF_CNPJ {
+
+	private final static int CNPJ_SIZE = 14;
+	private final static int CPF_SIZE = 11;
+
+	public static String execute(String cnpj_cpf) {
+
+		String retCode = "erro";
+
+		String string_cnpj_cpf = executeSplit(cnpj_cpf);
+
+		boolean soNumeros = isOnlyNumbers(string_cnpj_cpf);
+
+		if (!soNumeros) {
+			return retCode;
+		}
+
+		switch (checkCnpjCpf(string_cnpj_cpf)) {
+		case "CNPJ":
+			retCode = checkIfValidCnpj(string_cnpj_cpf);
+			break;
+
+		case "CPF":
+			retCode = checkIfValidCpf(string_cnpj_cpf);
+			break;
+		}
+
+		return retCode;
+	}
+
+	private static String checkIfValidCpf(String string_cnpj_cpf) {
+		if (validaCpf(string_cnpj_cpf)) {
+			return "SUCCESS";
+		}
+		return "ERRO";
+	}
+
+	private static boolean validaCpf(String string_cnpj_cpf) {
+
+		if (string_cnpj_cpf.equals("00000000000") || string_cnpj_cpf.equals("11111111111")
+				|| string_cnpj_cpf.equals("22222222222") || string_cnpj_cpf.equals("33333333333")
+				|| string_cnpj_cpf.equals("44444444444") || string_cnpj_cpf.equals("55555555555")
+				|| string_cnpj_cpf.equals("66666666666") || string_cnpj_cpf.equals("77777777777")
+				|| string_cnpj_cpf.equals("88888888888") || string_cnpj_cpf.equals("99999999999"))
+			return false;
+
+		char dig10, dig11;
+		int sm, i, r, num, peso;
+
+		try {
+			sm = 0;
+			peso = 10;
+			for (i = 0; i < 9; i++) {
+
+				num = (int) (string_cnpj_cpf.charAt(i) - 48);
+				sm = sm + (num * peso);
+				peso = peso - 1;
+			}
+
+			r = 11 - (sm % 11);
+			if ((r == 10) || (r == 11))
+				dig10 = '0';
+			else
+				dig10 = (char) (r + 48);
+
+			sm = 0;
+			peso = 11;
+			for (i = 0; i < 10; i++) {
+				num = (int) (string_cnpj_cpf.charAt(i) - 48);
+				sm = sm + (num * peso);
+				peso = peso - 1;
+			}
+
+			r = 11 - (sm % 11);
+			if ((r == 10) || (r == 11))
+				dig11 = '0';
+			else
+				dig11 = (char) (r + 48);
+
+			if ((dig10 == string_cnpj_cpf.charAt(9)) && (dig11 == string_cnpj_cpf.charAt(10)))
+				return true;
+			else
+				return false;
+		} catch (InputMismatchException erro) {
+			return false;
+		}
+	}
+
+	private static String checkIfValidCnpj(String string_cnpj_cpf) {
+
+		if (validaCnpj(string_cnpj_cpf)) {
+			return "SUCCESS";
+		}
+
+		return "ERRO";
+	}
+
+	private static boolean validaCnpj(String string_cnpj_cpf) {
+		if (string_cnpj_cpf.equals("00000000000000") || string_cnpj_cpf.equals("11111111111111")
+				|| string_cnpj_cpf.equals("22222222222222") || string_cnpj_cpf.equals("33333333333333")
+				|| string_cnpj_cpf.equals("44444444444444") || string_cnpj_cpf.equals("55555555555555")
+				|| string_cnpj_cpf.equals("66666666666666") || string_cnpj_cpf.equals("77777777777777")
+				|| string_cnpj_cpf.equals("88888888888888") || string_cnpj_cpf.equals("99999999999999"))
+			return false;
+
+		char dig13, dig14;
+		int sm, i, r, num, peso;
+
+		try {
+			sm = 0;
+			peso = 2;
+			for (i = 11; i >= 0; i--) {
+
+				num = (int) (string_cnpj_cpf.charAt(i) - 48);
+				sm = sm + (num * peso);
+				peso = peso + 1;
+				if (peso == 10)
+					peso = 2;
+			}
+
+			r = sm % 11;
+			if ((r == 0) || (r == 1))
+				dig13 = '0';
+			else
+				dig13 = (char) ((11 - r) + 48);
+
+			sm = 0;
+			peso = 2;
+			for (i = 12; i >= 0; i--) {
+				num = (int) (string_cnpj_cpf.charAt(i) - 48);
+				sm = sm + (num * peso);
+				peso = peso + 1;
+				if (peso == 10)
+					peso = 2;
+			}
+
+			r = sm % 11;
+			if ((r == 0) || (r == 1))
+				dig14 = '0';
+			else
+				dig14 = (char) ((11 - r) + 48);
+
+			if ((dig13 == string_cnpj_cpf.charAt(12)) && (dig14 == string_cnpj_cpf.charAt(13)))
+				return true;
+			else
+				return false;
+		} catch (InputMismatchException erro) {
+			return false;
+		}
+	}
+
+	private static String checkCnpjCpf(String string_cnpj_cpf) {
+
+		if (string_cnpj_cpf.length() == CNPJ_SIZE)
+			return "CNPJ";
+		else if (string_cnpj_cpf.length() == CPF_SIZE)
+			return "CPF";
+		else
+			System.out.println("Número de caracteres inválido");
+		return "INVALID";
+
+	}
+
+	private static boolean isOnlyNumbers(String string_cnpj_cpf) {
+		
+		return true;
+	}
+
+	private static String executeSplit(String cnpj_cpf) {
+
+		String output_cnpj_cpf = cnpj_cpf.replaceAll("[^0-9]+", "");
+		System.out.println(output_cnpj_cpf);
+
+		return output_cnpj_cpf;
+	}
+
+}
